d18791d2b09659c47d012e7fa201c8ebe2262527
Sebastoão Jr <herminigildoalisebastiao@gmail.com>
2025-04-08 02:56:36 +0200

    Add: criacao de novas premissoes

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -20,28 +20,7 @@ class RoleSeeder extends Seeder
                 'guard_name' => 'web',
                 'created_at' => '2023-11-16 21:32:55',
                 'updated_at' => '2023-11-16 21:32:55'
-            ],
-            [
-                'id' => 2,
-                'name' => 'sanitation technique',
-                'guard_name' => 'web',
-                'created_at' => '2023-11-17 12:27:21',
-                'updated_at' => '2023-11-17 12:27:23'
-            ],
-            [
-                'id' => 3,
-                'name' => 'finance technique',
-                'guard_name' => 'web',
-                'created_at' => '2023-11-21 09:00:40',
-                'updated_at' => '2023-11-21 09:00:42'
-            ],
-            [
-                'id' => 4,
-                'name' => 'administrative technique',
-                'guard_name' => 'web',
-                'created_at' => '2023-11-29 12:24:50',
-                'updated_at' => '2023-11-29 12:24:57'
-            ],
+            ]
         ];
 
         DB::table('roles')->insert($roles);
