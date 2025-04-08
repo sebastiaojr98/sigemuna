49d6653fd68bfa7b3cc2ec1d3978cb1a5f2d193c
Sebastoão Jr <herminigildoalisebastiao@gmail.com>
2025-04-08 05:00:10 +0200

    Add: Remocao da Logo da Falcon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -59,8 +59,8 @@
                     <div class="d-flex align-items-center">
                       <div class="toggle-icon-wrapper">
                         <button class="btn navbar-toggler-humburger-icon navbar-vertical-toggle" data-bs-toggle="tooltip" data-bs-placement="left" title="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
-                      </div><a class="navbar-brand" href="index.html">
-                        <div class="d-flex align-items-center py-3"><img class="me-2" src="assets/img/icons/spot-illustrations/falcon.png" alt="" width="25" /><span class="font-sans-serif">SIGEMUNA</span></div>
+                      </div><a class="navbar-brand" href="#">
+                        <div class="d-flex align-items-center py-3"><span class="font-sans-serif">SIGEMUNA</span></div>
                       </a>
                     </div>
                     <div class="collapse navbar-collapse" id="navbarVerticalCollapse">
